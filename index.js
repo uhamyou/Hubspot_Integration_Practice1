f0e2c3af544cbff37c1b7d4c3ca168ac974995ae
uhamyou <a_tkyng@hotmail.co.jp>
2025-01-28 13:11:56 +0900

    table formatted.

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 require('dotenv').config();
 const express = require('express');
 const axios = require('axios');
+const path = require('path');
 const app = express();
 
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
@@ -14,20 +16,23 @@ app.use(express.json());
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
 if (!PRIVATE_APP_ACCESS) {
     throw new Error('Missing PRIVATE_APP_ACCESS environment variable.');
-}
+};
 const PORT = 3000;
 
 // Retrieve all contacts
 app.get('/', async (req, res) => {
-    const contacts = 'https://api.hubspot.com/crm/v3/objects/contacts';
+    const petsEndpoint = 'https://api.hubspot.com/crm/v3/objects/pets?properties=name,pet_type,weight,age';
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
     }
+    const params = {
+        properties: ['pet_name', 'type', 'weight', 'age']
+    }
     try {
-        const resp = await axios.get(contacts, { headers });
-        const data = resp.data.results;
-        res.render('index', { title: 'Contacts | HubSpot APIs', data });      
+        const response = await axios.get(petsEndpoint, { headers, params });
+        const pets = response.data.results;
+        res.render('index', { title: 'List of Pets', pets: pets });
     } catch (error) {
         console.error(error);
     }
